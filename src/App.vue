76bee612cd0a567e8900884add9f63758fba99a8
miguel <meqh1992@gmail.com>
2024-03-28 16:53:45 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {  RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  <div>
+    <h1 class="text-fuchsia-400	">Hola mundo</h1>
+  </div>
+  </header>
+
+
+</template>
+
+<style scoped>
+
+</style>
