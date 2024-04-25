d350e524c5d7f2c758ff58bcbfa753ba72ee8910
miguel <meqh1992@gmail.com>
2024-04-24 21:57:19 -0500

    patch: remove import

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import userStorage from "@/stores/userStorage";
 import {useToast} from "primevue/usetoast";
-import {data} from "autoprefixer";
 const toast = useToast();
 
 
